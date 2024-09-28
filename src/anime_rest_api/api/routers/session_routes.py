054bd53ec4863df65b3b7e336a5c7c002d103008
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-09-28 11:43:39 -0700

    add: explicitly list out the expiration times for given tokens

--- src/anime_rest_api/api/routers/session_routes.py
+++ src/anime_rest_api/api/routers/session_routes.py
@@ -8,11 +8,9 @@ from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
 from fastapi.responses import PlainTextResponse
-from fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from anime_rest_api.api.common_query import requesting_user_header
-from anime_rest_api.api.common_query import security
 from anime_rest_api.api.dependencies import DbDependency
 from anime_rest_api.api.models import LoginRequest
 from anime_rest_api.api.models import LoginResponse
@@ -20,11 +18,12 @@ from anime_rest_api.api.models import RefreshRequest
 from anime_rest_api.api.models import RefreshResponse
 from anime_rest_api.api.models.sessions import JWTError
 from anime_rest_api.api.models.sessions import JwtUser
+from anime_rest_api.api.models.sessions import access_token_claims_from_user
 from anime_rest_api.api.models.sessions import build_access_token
 from anime_rest_api.api.models.sessions import build_refresh_token
-from anime_rest_api.api.models.sessions import decode_access_token
 from anime_rest_api.api.models.sessions import decode_refresh_token
 from anime_rest_api.api.models.sessions import epoch_now
+from anime_rest_api.api.models.sessions import refresh_token_claims_from_user
 from anime_rest_api.db.crud.errors import EntryNotFoundError
 from anime_rest_api.db.crud.user_operations import get_user
 from anime_rest_api.db.crud.user_operations import get_user_login
@@ -48,11 +47,19 @@ async def login_route(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid username or password",
         ) from e
-    access_token = build_access_token(user)
-    refresh_token = build_refresh_token(user, access_token=access_token)
+    user_access_claims = access_token_claims_from_user(user)
+    user_refresh_claims = refresh_token_claims_from_user(user)
+    access_token = build_access_token(user_access_claims.model_dump(by_alias=True))
+    refresh_token = build_refresh_token(
+        user_refresh_claims.model_dump(by_alias=True),
+        access_token=access_token,
+    )
     return {
         "access_token": access_token,
+        "expires_at": user_access_claims.expires_at,
         "refresh_token": refresh_token,
+        "refresh_expires_at": user_refresh_claims.expires_at,
+        "version": user.session_version,
     }
 
 
@@ -60,13 +67,11 @@ async def login_route(
 async def refresh_route(
     tokens: RefreshRequest,
     session: Annotated[AsyncSession, DbDependency],
-    user_credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
 ):
     """Refresh a user session."""
     try:
         decoded = decode_refresh_token(
             tokens.refresh_token,
-            user_credentials.credentials,
         )
     except JWTError as e:
         LOG.exception("Invalid refresh token")
@@ -79,11 +84,7 @@ async def refresh_route(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid refresh token",
         )
-    requesting_user = decode_access_token(
-        user_credentials.credentials,
-        verify_exp=False,
-    )
-    user = await get_user(session, int(requesting_user.user_id))
+    user = await get_user(session, int(decoded.user_id))
     # make sure that this refresh token is valid for current login session
     if user.session_version != decoded.session_version:
         LOG.error(
@@ -95,11 +96,19 @@ async def refresh_route(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid refresh token",
         )
-    access_token = build_access_token(user)
-    refresh_token = build_refresh_token(user, access_token=access_token)
+    user_access_claims = access_token_claims_from_user(user)
+    user_refresh_claims = refresh_token_claims_from_user(user)
+    access_token = build_access_token(user_access_claims.model_dump(by_alias=True))
+    refresh_token = build_refresh_token(
+        user_refresh_claims.model_dump(by_alias=True),
+        access_token=access_token,
+    )
     return {
         "access_token": access_token,
+        "expires_at": user_access_claims.expires_at,
         "refresh_token": refresh_token,
+        "refresh_expires_at": user_refresh_claims.expires_at,
+        "version": user.session_version,
     }
 
 
