fa6ada84955b4527c5b58c9073dede851edd4475
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-09-28 11:43:03 -0700

    add: separate the model of reading a user from database vs what clients see

--- src/anime_rest_api/api/models/users.py
+++ src/anime_rest_api/api/models/users.py
@@ -1,6 +1,6 @@
 from pydantic import computed_field
 
-from anime_rest_api.db.models.auth import UserRead
+from anime_rest_api.db.models.auth import UserPublic
 
 from .base import Base
 
@@ -8,7 +8,7 @@ from .base import Base
 class UserResponseList(Base):
     """Response model for a list of shows."""
 
-    users: list[UserRead]
+    users: list[UserPublic]
     has_more: bool
 
     @computed_field
