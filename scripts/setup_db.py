fdb56727be5ccd710e2a519058a303c71448cbba
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-09-29 00:05:03 -0700

    fix: use new import paths for necessary objects

--- scripts/setup_db.py
+++ scripts/setup_db.py
@@ -4,26 +4,32 @@ import asyncio
 import os
 import sys
 
+from sqlalchemy.schema import DropSchema
+from sqlalchemy.ext.asyncio import AsyncConnection
+
+from anime_rest_api.db import DatabaseConnection
+from anime_rest_api.db import setup_db, AUTH_METADATA, CONTENT_METADATA
+
+
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser("setup_db")
     parser.add_argument("database_url", metavar="<URL>")
     return parser.parse_args()
 
+async def clean_db(conn: AsyncConnection) -> None:
+    for meta in [CONTENT_METADATA, AUTH_METADATA]:
+        await conn.run_sync(meta.drop_all)
+        await conn.execute(DropSchema(meta.schema, cascade=True))
 
-def main(args: argparse.Namespace) -> int:
-    os.environ["ANIME_API_DATABASE_URL"] = args.database_url
-    from anime_rest_api.db import DatabaseConnection
-    from anime_rest_api.db import setup_db, clean_db
-    async def _db_calls(dbc: "DatabaseConnection") -> None:
-        async with dbc.engine.begin() as conn:
-            await clean_db(conn)
-            await setup_db(conn)
-    del DatabaseConnection._instance
+
+async def main(args: argparse.Namespace) -> int:
     dbc = DatabaseConnection(args.database_url, echo=True)
-    asyncio.run(_db_calls(dbc))
+    async with dbc.engine.begin() as conn:
+        await clean_db(conn)
+        await setup_db(conn)
     return 0
 
 
 if __name__ == "__main__":
     args = get_args()
-    sys.exit(main(args))
+    sys.exit(asyncio.run(main(args)))
