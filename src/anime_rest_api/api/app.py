ef390fb51d1fbe5d8d6d9e3bf19c52e1d60d66f7
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-09-29 11:42:49 -0700

    add: logging setup at app startup

--- src/anime_rest_api/api/app.py
+++ src/anime_rest_api/api/app.py
@@ -1,9 +1,12 @@
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
+import logging
+import logging.config
 
 from fastapi import FastAPI
 
 from anime_rest_api import __version__
+from anime_rest_api.api.log import LogConfig
 from anime_rest_api.api.routers import SHOW_ROUTER
 from anime_rest_api.api.routers import USER_ROUTER
 from anime_rest_api.db import setup_db
@@ -13,6 +16,9 @@ from anime_rest_api.db.connection import Db
 @asynccontextmanager
 async def lifespan(_app: FastAPI) -> AsyncGenerator[None]:
     """Run on-startup and on-shutdown code."""
+    logging.config.dictConfig(
+        LogConfig(LOGGER_NAME="anime-api", LOG_LEVEL="DEBUG").model_dump(),
+    )
     async with Db.engine.begin() as conn:
         await setup_db(conn)
     yield
